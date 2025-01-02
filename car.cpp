8ae6721998613f85eba4040a3de4d9ea43636044
yedididi <iyeji366@gmail.com>
2025-01-02 09:09:18 +0900

    wall crash ok

--- car.cpp
+++ car.cpp
@@ -1,5 +1,6 @@
 #include "defined.h"
 #include "car.h"
+#include "device_driver.h"
 
 car::car(int x, int y, int w, int h, int ci, int dir) : object(x, y, w, h, ci, dir)
 {
@@ -7,7 +8,12 @@ car::car(int x, int y, int w, int h, int ci, int dir) : object(x, y, w, h, ci, d
 
 void car::Car_Move(char map[24][32])
 {
-    if (map[this->y / 10][(this->x + (CAR_STEP * this->dir))/ 10] == '1')
+    if (map[this->y / 10][(this->x + (CAR_STEP * this->dir)) / 10] == '1')
+    {
+        Uart_Printf("x is:%d\n", this->x);
+        this->dir = -this->dir;
+    }
+    else if (((this->x + (CAR_STEP * this->dir)) % 10 != 0) && (map[this->y / 10][(this->x + (CAR_STEP * this->dir * 2)) / 10] == '1'))
         this->dir = -this->dir;
     else
         this->x += CAR_STEP * this->dir;
